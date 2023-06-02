5fe04b32993fa1a160058c45aee117bb9915c50f
Yura Minasyan <minas6161@gmail.com>
2023-06-02 16:27:22 +0400

    table change

--- app/src/main/java/com/example/nyam_nyamadmin/MainActivity.java
+++ app/src/main/java/com/example/nyam_nyamadmin/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.nyam_nyamadmin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
     TextView table;
@@ -13,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     Button table2;
     Button table3;
     Button table4;
+    private DatabaseReference mDatabase;
+    FirebaseFirestore Firestore;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +36,23 @@ public class MainActivity extends AppCompatActivity {
         table2 = findViewById(R.id.table2);
         table3 = findViewById(R.id.table3);
         table4 = findViewById(R.id.table4);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        Firestore = FirebaseFirestore.getInstance();
+        Firestore.collection("Order").document("dMGM3or0ycKo2UUkJ4tD").get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                String table = documentSnapshot.getString("Table");
+                String total = documentSnapshot.getString("Total");
+                if (Integer.parseInt(table) == 0){
+                    table1.setBackgroundColor(getResources().getColor(R.color.green));
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this,"Fail",Toast.LENGTH_LONG).show();
+            }
+        });
 //        table1.setBackgroundColor(getResources().getColor(R.color.green));
     }
 }
