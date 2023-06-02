974974527a22383b53e25e52956cf528775a2b9a
Yura Minasyan <minas6161@gmail.com>
2023-06-02 16:07:34 +0400

    changes

--- app/src/main/java/com/example/nyam_nyamadmin/MainActivity.java
+++ app/src/main/java/com/example/nyam_nyamadmin/MainActivity.java
@@ -4,16 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView table;
+    Button table1;
+    Button table2;
+    Button table3;
+    Button table4;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         table=findViewById(R.id.table);
-        table.setText("sad");
+        table1 = findViewById(R.id.table1);
+        table2 = findViewById(R.id.table2);
+        table3 = findViewById(R.id.table3);
+        table4 = findViewById(R.id.table4);
+//        table1.setBackgroundColor(getResources().getColor(R.color.green));
     }
 }
